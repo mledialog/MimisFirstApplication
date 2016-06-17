335516ade2d323c0463965eb251faab7d02bdb38
ml <ml>
2016-06-17 13:08:05 +0200

    Initial commit

---
+++ app/src/main/java/com/e_dialog/ml/mimisfirstapplication/MimisActivity.java
@@ -0,0 +1,18 @@
+package com.e_dialog.ml.mimisfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MimisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mimis);
+    }
+
+    /** Called when the user clicks the send button */
+    public void sendMessage(View view){
+        //Do something in response to button
+    }
+}
